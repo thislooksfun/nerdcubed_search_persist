0135250172f285fe3b6b4b5f7006d595f33c9cf5
thislooksfun <thislooksfun@users.noreply.github.com>
2016-12-04 20:34:21 -0600

    Update nerdcubed_search_persist.user.js

--- nerdcubed_search_persist.user.js
+++ nerdcubed_search_persist.user.js
@@ -3,9 +3,12 @@
 // @version      1.0
 // @description  Make searches on nerdcubed.co.uk persist across clicks
 // @author       thislooksfun
+// @license      GPL v3; https://github.com/thislooksfun/nerdcubed_search_persist/blob/master/LICENSE
 // @match        http://www.nerdcubed.co.uk/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
+// @homepageURL  https://github.com/thislooksfun/nerdcubed_search_persist
+// @supportURL   https://github.com/thislooksfun/nerdcubed_search_persist/issues
 // @downloadURL  https://github.com/thislooksfun/nerdcubed_search_persist/raw/master/nerdcubed_search_persist.user.js
 // ==/UserScript==
 
